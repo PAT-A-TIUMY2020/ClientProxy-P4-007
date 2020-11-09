5df7370de3b03cd8707228d9db11e0958690d5a9
istytsaniya <tsaniyaisty08@gmail.com>
2020-11-09 23:42:18 +0700

    Add try and catch module 6

--- ClientProxy-P4-20180140007/Program.cs
+++ ClientProxy-P4-20180140007/Program.cs
@@ -12,21 +12,39 @@ namespace ClientProxy_P4_20180140007
     {
         static void Main(string[] args)
         {
-            BasicHttpBinding bind = new BasicHttpBinding();
-            ChannelFactory<IMatematika> objChannel = new
-            ChannelFactory<IMatematika>(bind, "http://localhost:1907");
-            IMatematika obj = objChannel.CreateChannel();
-            double hasilTambah = obj.Tambah(1, 2);
-            Console.WriteLine("1 + 2 = " + hasilTambah);
-            double hasilKurang = obj.Kurang(3, 2);
-            Console.WriteLine("3 - 2 = " + hasilKurang);
-            double hasilKali = obj.Kali(2, 2);
-            Console.WriteLine("2 x 2 = " + hasilKali);
-            double hasilBagi = obj.Bagi(2, 2);
-            Console.WriteLine("2 / 2 = " + hasilBagi);
-            Console.ReadLine();
-            Koordinat objk = new Koordinat();
+            try
+            {
+                ServiceReference1.MatematikaClient obj = new ServiceReference1.MatematikaClient();
+                double hasilTambah = obj.Tambah(1, 2);
+                Console.WriteLine("1 + 2 = " + hasilTambah);
+                double hasilKurang = obj.Kurang(3, 2);
+                Console.WriteLine("3 - 2 = " + hasilKurang);
+                double hasilKali = obj.Kali(2, 2);
+                Console.WriteLine("2 x 2 = " + hasilKali);
+                double hasilBagi = obj.Bagi(2, 2);
+                Console.WriteLine("2 / 2 = " + hasilBagi);
 
+                ServiceReference1.Koordinat a = new
+                ServiceReference1.Koordinat();
+                ServiceReference1.Koordinat b = new
+                ServiceReference1.Koordinat();
+                a.X = 7;
+                a.Y = 8;
+
+                b.X = 5;
+                b.Y = 6;
+                double selisihX = a.X - b.X;
+                double selisihY = a.Y - b.Y;
+                double jarak = Math.Sqrt(Math.Pow(selisihX, 2) +
+                Math.Pow(selisihY, 2));
+                Console.WriteLine("Hasil Koordinat " + jarak);
+            }
+                catch (FaultException<ServiceReference1.MathFault> mf)
+            {
+                Console.WriteLine(mf.Detail.Kode);
+                Console.WriteLine(mf.Detail.Pesan);
+            }
+            Console.ReadLine();
         }
     }
 }
